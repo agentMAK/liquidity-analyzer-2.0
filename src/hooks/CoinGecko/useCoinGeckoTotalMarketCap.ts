a6e3aa804b0b55a15a9632ded835fe362b3fd42a
agentMAK <martin0044@gmail.com>
2023-01-31 12:00:23 +0000

    Coingecko Fix

--- src/hooks/CoinGecko/useCoinGeckoTotalMarketCap.ts
+++ src/hooks/CoinGecko/useCoinGeckoTotalMarketCap.ts
@@ -4,11 +4,11 @@ import { INDEX_TOKENS } from "@utils/constants/tokens";
 const useCoinGeckoTotalMarketCap = () => {
 
 const {data,isLoading,isError} = useCoinGeckoMarketData(Object.values(INDEX_TOKENS).map((token) => token.coinGeckoId));
-let totalMarketCap = 0;
+  let totalMarketCap = 0;
 
-Object.values(data).map((token:any) => {
-  totalMarketCap += parseInt(token.market_cap)
-})
+  Object.values(data || {}).map((token:any) => {
+    totalMarketCap += parseInt(token.market_cap)
+  })
 
 return {
   data: totalMarketCap,
